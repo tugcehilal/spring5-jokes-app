bc7f4c94bc1077134cff45850e1d4a0d28a510f2
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-20 19:02:16 +0300

    jokes app

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -3,10 +3,18 @@ package guru.springframework.joke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @SpringBootApplication
 public class JokeappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JokeappApplication.class, args);
 	}
+
+
+
+
 }
