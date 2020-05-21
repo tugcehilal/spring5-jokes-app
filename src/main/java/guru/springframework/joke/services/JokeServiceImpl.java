a8ff7eebf185505195fe4636987a824ef865f6c5
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-22 00:39:59 +0300

    Config

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -11,10 +11,14 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
+    //eski hali
+   /* public JokeServiceImpl() {
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
-    }
+    }*/
 
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
     @Override
     public String getJoke() {
         return chuckNorrisQuotes.getRandomQuote();
